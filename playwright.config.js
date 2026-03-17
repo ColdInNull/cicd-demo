93b0e5032b14e57d3bfdcbac836883d347aa46d7
qiankun <qiankunfaith@gmail.com>
2026-03-17 15:14:31 +0800

    feat: 添加e2e测试失败后上传截图的功能

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
   use: {
     baseURL: 'http://127.0.0.1:4173',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
   webServer: {
     command: 'pnpm exec vite --host 127.0.0.1 --port 4173 --strictPort',
